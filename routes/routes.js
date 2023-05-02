bae4ebf6e4f7b2af63db9b11c4a8d9c0c0171a01
Zhor <zhor@ThinkPad-L14-Gen-2-d7727f99.localdomain>
2023-05-02 18:10:37 +0300

    add delete data by id

--- routes/routes.js
+++ routes/routes.js
@@ -53,7 +53,14 @@ catch (error){
 }
 })
 
-router.delete('/delete/:id', (req, res) =>{
-    res.send('Delete by ID API')
+router.delete('/delete/:id', async (req, res) =>{
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error){
+        res.status(400).json({message: error.message})
+    }
 })
 module.exports = router;

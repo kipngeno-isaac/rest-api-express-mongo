cf91069ef8875d0d074cb988981636e7c9e32009
Zhor <zhor@ThinkPad-L14-Gen-2-d7727f99.localdomain>
2023-05-02 20:45:54 +0300

    add controller

---
+++ src/controller/controller.js
@@ -0,0 +1,62 @@
+const Model = require('../model/model');
+
+exports.postData = async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
+
+}
+
+exports.getAllData = async (req, res)=>{
+    try {
+        const data = await Model.find();
+        res.json(data)
+    }
+    catch (error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+exports.getDataById = async (req, res)=>{
+    try {
+        const data = await Model.findById(req.params.id);
+        res.json(data)
+    }
+    catch (error){
+        res.status(500).json({message: error.message})
+    }
+}
+
+exports.updateData = async (req, res)=>{
+    try {
+        const id = req.params.id;
+        const updateData = req.body;
+        const options = {new: true};
+
+        const result = await Model.findByIdAndUpdate(id, updateData, options)
+
+        res.send(result)
+    }
+    catch (error){
+        res.status(400).json({message: error.message})
+    }
+}
+
+exports.deleteData = async (req, res) =>{
+    try {
+        const id = req.params.id;
+        const data = await Model.findByIdAndDelete(id)
+        res.send(`Document with ${data.name} has been deleted..`)
+    }
+    catch (error){
+        res.status(400).json({message: error.message})
+    }
+}

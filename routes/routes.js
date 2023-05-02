7fa6bd30e69d1535b7fb1baccfd0329b23044796
Zhor <zhor@ThinkPad-L14-Gen-2-d7727f99.localdomain>
2023-05-02 17:39:16 +0300

    add routes

---
+++ routes/routes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const Model = require('../model/model');
+const router = express.Router()
+
+router.post('/post', async (req, res) =>{
+    const data = new Model({
+        name: req.body.name,
+        age: req.body.age
+    })
+
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave)
+    }
+    catch (error){
+        res.status(400).json({message: error.message})
+    }
+
+})
+
+router.get('/getAll', async (req, res)=>{
+try {
+    const data = await Model.find();
+    res.json(data)
+}
+catch (error){
+    res.status(500).json({message: error.message})
+}
+})
+
+router.get('/getOne/:id', (req, res)=>{
+
+    res.send(req.params.id)
+})
+
+router.patch('/update/:id', (req, res)=>{
+    res.send('Update by ID API')
+})
+
+router.delete('/delete/:id', (req, res) =>{
+    res.send('Delete by ID API')
+})
+module.exports = router;
